84cab8e9ac0816a7ba20245f836134cedf5477d6
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2024-09-25 14:29:54 +0100

    prerender off

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,5 +2,5 @@ export const config = {
 	runtime: 'edge'
 };
 
-export const prerender = true; //when true, headers will not be added
+export const prerender = false; //when true, headers will not be added
 export const ssr = true;
